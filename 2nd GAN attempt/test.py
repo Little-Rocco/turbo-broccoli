695f398640061ceca359f5416628170ec4322080
schoolshooter69 <57626329+schoolshooter69@users.noreply.github.com>
2021-11-03 15:46:47 +0100

    dcgan
    test.py is the dcgan

--- 2nd GAN attempt/test.py
+++ 2nd GAN attempt/test.py
@@ -24,6 +24,18 @@ batch_size = 64
 image_size = 64
 
 # Number of channels in the training images. For color images this is 3
+nc = 3
+
+# Size of z latent vector (i.e. size of generator input)
+nz = 100
+
+# Size of feature maps in generator
+ngf = 64
+
+# Size of feature maps in discriminator
+ndf = 64
+
+# Number of channels in the training images. For color images this is 3
 colour_channels = 3
 
 # Size of z latent vector (i.e. size of generator input)
@@ -39,10 +51,10 @@ learning_rate = 0.0002
 beta1_hyperparam = 0.5
 
 # Number of epochs to wait before showing graphs
-iters_between_each_graph = 3166*5
+iters_between_each_graph = 3166
 
 # Number of iterations to wait before printing updates
-iters_between_updates = iters_between_each_graph/25
+iters_between_updates = 50 #iters_between_each_graph/25
 
 dataset = torchvision.datasets.ImageFolder(root=dataroot,
                                            transform=torchvision.transforms.Compose([
@@ -65,21 +77,29 @@ class Generator(torch.nn.Module):
         super(Generator, self).__init__()
         self.flatten = torch.nn.Flatten()
         self.linear_relu_stack = torch.nn.Sequential(
-            # the neural network
-            torch.nn.Linear(gen_input_nodes, 256),
-            torch.nn.LeakyReLU(0.2, inplace=True),
-            torch.nn.Linear(256, 512),
-            torch.nn.LeakyReLU(0.2, inplace=True),
-            torch.nn.Linear(512, 1024),
-            torch.nn.LeakyReLU(0.2, inplace=True),
-            torch.nn.Linear(1024, 2048),
-            torch.nn.LeakyReLU(0.2, inplace=True),
-            torch.nn.Linear(2048, image_size * image_size * colour_channels),
-            torch.nn.Tanh(),
+            # input is Z, going into a convolution
+            torch.nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
+            torch.nn.BatchNorm2d(ngf * 8),
+            torch.nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            torch.nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            torch.nn.BatchNorm2d(ngf * 4),
+            torch.nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            torch.nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            torch.nn.BatchNorm2d(ngf * 2),
+            torch.nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            torch.nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
+            torch.nn.BatchNorm2d(ngf),
+            torch.nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            torch.nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
+            torch.nn.Tanh()
+            # state size. (nc) x 64 x 64
         )
 
     def forward(self, x):
-        x = self.flatten(x)
         logits = self.linear_relu_stack(x).reshape((x.shape[0], colour_channels, image_size, image_size))
         return logits
 
@@ -94,16 +114,27 @@ class Discriminator(torch.nn.Module):
         super(Discriminator, self).__init__()
         self.flatten = torch.nn.Flatten()
         self.linear_relu_stack = torch.nn.Sequential(
-            torch.nn.Linear(image_size * image_size * colour_channels, 512),
+            # input is (nc) x 64 x 64
+            torch.nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
             torch.nn.LeakyReLU(0.2, inplace=True),
-            torch.nn.Linear(512, 256),
+            # state size. (ndf) x 32 x 32
+            torch.nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            torch.nn.BatchNorm2d(ndf * 2),
             torch.nn.LeakyReLU(0.2, inplace=True),
-            torch.nn.Linear(256, 1),
-            torch.nn.Sigmoid(),
+            # state size. (ndf*2) x 16 x 16
+            torch.nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            torch.nn.BatchNorm2d(ndf * 4),
+            torch.nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            torch.nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            torch.nn.BatchNorm2d(ndf * 8),
+            torch.nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4
+            torch.nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            torch.nn.Sigmoid()
         )
 
     def forward(self, x):
-        x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
 

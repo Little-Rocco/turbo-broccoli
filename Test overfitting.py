6e98cc94b84d3bb3e445d96067e6181dd8164304
Bob <54476467+BobbyMcdonald@users.noreply.github.com>
2021-11-26 11:36:53 +0100

    Create Test overfitting.py
    test for overfitting

---
+++ Test overfitting.py
@@ -0,0 +1,83 @@
+#to-do: add a range for when we want to observe if two images are similar
+
+# NOTE: Before you can run it you need to make sure you have the below packages
+
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage import io
+import os
+
+#includes the functions used for calculating the
+#structural similarity (structural simelarity index measure (ssim) ) between two images
+from skimage.metrics import structural_similarity as ssim
+
+#used for resizing
+import cv2
+
+#calculates the mean squared error between two images - sum of the squared difference between the two images
+def mean_squared_error(imageA, imageB):
+
+    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    err /= float(imageA.shape[0] * imageB.shape[1])
+    return err
+
+def compare_images(imageA, imageB, title):
+    # resize images - only needed if the images have different dimensions
+    res1 = cv2.resize(imageA, dsize=(54, 140), interpolation=cv2.INTER_CUBIC)
+    res2 = cv2.resize(imageB, dsize=(54, 140), interpolation=cv2.INTER_CUBIC)
+
+    #calculate mean squared error
+    m = mean_squared_error(res1, res2)
+
+    # calculate structural simelarity
+    s = ssim(res1, res2, multichannel=True)
+
+    # setup the figure
+    fig = plt.figure(title)
+    plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
+
+    # show first image
+    ax = fig.add_subplot(1, 2, 1)
+    plt.imshow(imageA, cmap=plt.cm.gray)
+    plt.axis("off")
+
+    # show the second image
+    ax = fig.add_subplot(1, 2, 2)
+    plt.imshow(imageB, cmap=plt.cm.gray)
+    plt.axis("off")
+
+    plt.show
+
+#folder containing the generated images
+generated_images = "C:\\Users\\fred7\\Documents\\GitHub\\turbo-broccoli\\generated images"
+
+#needs to be linked to the CelebA faces dataset
+real_images = "C:\\Users\\fred7\\Documents\\GitHub\\turbo-broccoli\\CelebA dataset\\img_align_celeba"
+
+#iterate over real and generated images
+for file_name1 in os.listdir(real_images):
+    for file_name2 in os.listdir(generated_images):
+
+        #read generated images
+        filepath1 = os.path.join(generated_images, file_name2)
+        image01 = io.imread(filepath1)
+
+        #read real images
+        filepath2 = os.path.join(real_images, file_name1)
+        image02 = io.imread(filepath2)
+
+        #create a tuble that we can iterate through - here a title of the specific image is specified too
+        fig = plt.figure("Images")
+        images = ("generated image", image01), ("real_image", image02)
+
+        # loop over the tuble containging the generated and real images
+        for (i, (name, image)) in enumerate(images):
+
+            ax = fig.add_subplot(1, 3, i + 1)
+            ax.set_title(name)
+            plt.imshow(image, cmap = plt.cm.gray)
+            plt.axis("off")
+            plt.show()
+
+            # compare the generated and real images
+            compare_images(image01, image02, "generated image vs. real image")

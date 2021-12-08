214cb4abe33613d861280aa2c8ff5d93da5c66f9
schoolshooter69 <57626329+schoolshooter69@users.noreply.github.com>
2021-12-08 14:58:08 +0100

    update lsdc, it actually works

--- latenspacedifferenceCalculator.py
+++ latenspacedifferenceCalculator.py
@@ -5,12 +5,11 @@ import os
 
 from torch.autograd import variable
 
-#MemoryLeak, not sure if this works
+#too many indices for tensor of dim 4
 
 path = 'Refactored' + os.path.sep + 'LatentSpace' + os.path.sep
 i = 0
-latentSpaceAverage = torch.empty((2, 100, 1, 1))
-latentSpaceAverage.detach()
+latentSpaceAverage = torch.zeros((64, 100, 1, 1))
 
 
 x1 = os.walk(path)
@@ -18,12 +17,12 @@ x2 = list(x1)
 x3 = x2[0]
 fileNames = x3[2]
 for fName in fileNames:
-   latentSpace = torch.load(path + os.path.sep + fName)['latentSpace']
-   latentSpace.detach()
+   latentSpace = torch.load(path +  os.path.sep + fName)['latentSpace']
+   torch.isnan(latentSpace)
    latentSpaceAverage += latentSpace
    i += 1
 
 
 
 latentSpaceAverage = latentSpaceAverage/i
-print(latentSpaceAverage)
+print('this is some average i hope: \n' + str(latentSpaceAverage))

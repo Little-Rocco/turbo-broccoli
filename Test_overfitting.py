70cc3371f3e22dc71221f17d660e56552ad2d998
Bob <54476467+BobbyMcdonald@users.noreply.github.com>
2021-12-20 19:45:35 +0100

    new stuff

--- Test_overfitting.py
+++ Test_overfitting.py
@@ -1,14 +1,9 @@
-# Documentation
-# Before you can run it you need to make sure you have the below packages
+# NOTE: Before you can run it you need to make sure you have the below packages
 #           - opencv (imported as cv2)
 #               - using pip: https://www.tutorialspoint.com/how-to-install-opencv-in-python
 #               - using conda: conda install -c conda-forge opencv
 #           - scikit-image (imported as skimage)
 #               - https://scikit-image.org/docs/dev/install.html
-# Code inspired by: https://scikit-image.org/docs/dev/auto_examples/transform/plot_ssim.html 
-# 
-# End of documentation
-
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -19,29 +14,25 @@ import os
 #structural similarity (structural simelarity index measure or SSIM) between two images
 from skimage.metrics import structural_similarity as ssim
 
+#includes mean squared error to measure difference in pixels
+from skimage.metrics import mean_squared_error
+
 #used for resizing
 import cv2
 
 #folder containing the generated images
-generated_images = "C:\\Users\\Frederik\\Documents\\GitHub\\turbo-broccoli\\test"
+generated_images = "C:\\Users\\fred7\\Documents\\GitHub\\turbo-broccoli\\test_images"
 
 #folder containing the CelebA faces dataset
-real_images = "C:\\Users\\Frederik\\Documents\\GitHub\\turbo-broccoli\\celebafaces\\img_align_celeba\\img_align_celeba"
+real_images = "C:\\Users\\fred7\\Documents\\GitHub\\turbo-broccoli\\test_images"
 
 # values for checking the desired range of similarity between the real and generated images
-checkM = 3000
-checkS = 0.8
+checkM = 6000
+checkS = 0.5
 
-#calculates the mean squared error between two images - sum of the squared difference between the two images
-def mean_squared_error(imageA, imageB):
+i=0
 
-    mse = np.square(imageA.astype("float") - imageB.astype("float"))
-    mse = np.sum(mse)
-    mse = np.mean(mse / float(imageA.shape[0] * imageB.shape[1]))
-
-    return mse
-
-def compare_images(imageA, imageB, title):
+def compare(imageA, imageB, title):
     #resize images - only needed if the images have different dimensions
     res1 = cv2.resize(imageA, dsize=(54, 140), interpolation=cv2.INTER_CUBIC)
     res2 = cv2.resize(imageB, dsize=(54, 140), interpolation=cv2.INTER_CUBIC)
@@ -52,20 +43,39 @@ def compare_images(imageA, imageB, title):
     #calculate structural similarity
     s = ssim(res1, res2, multichannel=True)
 
-    if m <= checkM or s >= checkS:
-        #setup the figure
-        fig = plt.figure(title)
-        plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
+    # #im1 = cv2.imread(imageA, 0)
+    # hist1 = cv2.calcHist([imageA], [0], None, [256], [0, 256])
+    #
+    # #im2 = cv2.imread(imageB, 0)
+    # hist2 = cv2.calcHist([imageB], [0], None, [256], [0, 256])
+    #
+    # a = cv2.compareHist(hist1, hist2, cv2.HISTCMP_BHATTACHARYYA)
 
-        #show first image
-        ax = fig.add_subplot(1, 2, 1)
-        plt.imshow(imageA, cmap=plt.cm.gray)
-        plt.axis("off")
+    #if m <= checkM or s >= checkS:
+    #setup the figure
+    fig = plt.figure(title)
+    plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
+    # plt.suptitle("MSE: %.2f, SSIM: %.2f, HIST: %.2f" % (m, s, a))
 
-        #show the second image
-        ax = fig.add_subplot(1, 2, 2)
-        plt.imshow(imageB, cmap=plt.cm.gray)
-        plt.axis("off")
+    #show first image
+    ax = fig.add_subplot(1, 2, 1)
+    plt.imshow(imageA, cmap=plt.cm.gray)
+    plt.axis("off")
+
+    #show the second image
+    ax = fig.add_subplot(1, 2, 2)
+    plt.imshow(imageB, cmap=plt.cm.gray)
+    plt.axis("off")
+
+    #plt.savefig("C:\\Users\\fred7\\Documents\\GitHub\\turbo-broccoli\\Refactored\\sus images\\" + "sus_comparison" + str(i) + ".png")
+
+
+from datetime import datetime
+
+time1 = datetime.now()
+
+#time1 = time1.strftime("%H:%M:%S")
+
 
 #iterate over real and generated images
 for file_name1 in os.listdir(real_images):
@@ -79,10 +89,21 @@ for file_name1 in os.listdir(real_images):
         filepath2 = os.path.join(real_images, file_name1)
         image02 = io.imread(filepath2)
 
-        plt.show()
+        #plt.show()
 
         #prints the below message for every comparison
-        print("I am still runnning :)")
+        if i % 1000 == 0:
+            print("I am still runnning :)", i, "comparisons")
 
         #compare the generated and real images
-        compare_images(image01, image02, "generated image vs. real image")
+        compare(image01, image02, "generated image vs. real image")
+
+        i = i + 1
+
+time2 = datetime.now()
+
+#time2 = time2.strftime("%H:%M:%S")
+
+final_time = time2 - time1
+
+print(final_time.total_seconds())
